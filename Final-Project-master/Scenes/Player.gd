b07c4e728b84ed7942fdd2d7e31c47b8cf4103c1
rifhall <rifhall@iu.edu>
2020-04-29 20:26:03 -0400

    workin on it

--- Final-Project-master/Scenes/Player.gd
+++ Final-Project-master/Scenes/Player.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-onready var Bullet = load("res://Scenes/Explosion.tscn")
+onready var Bullet = load("res://Scenes/Bullet.tscn")
 const MOVE_SPEED = 300
 var health = 3
 var score = 0
@@ -21,6 +21,7 @@ func _physics_process(delta):
 		move_vec.x -= 1
 	if Input.is_action_pressed("move_right"):
 		move_vec.x += 1
+		
 	move_vec = move_vec.normalized()
 	move_and_collide(move_vec * MOVE_SPEED * delta)
    
@@ -41,3 +42,10 @@ func kill():
 	health -= 1
 	if health <=0:
 		get_tree().reload_current_scene()
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Zombie":
+		health -=1
+		if health <= 0:
+			get_tree().reload_current_scene()
